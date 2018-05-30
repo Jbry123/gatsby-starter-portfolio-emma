54aedffe3ad824e8cbf9f2e7bec669127a0b2d4d
Unknown <jonathanbry@comcast.net>
2018-05-30 01:11:38 -0700

    fixed header and videoBox

--- src/components/VideoBox/VideoBox.jsx
+++ src/components/VideoBox/VideoBox.jsx
@@ -9,62 +9,66 @@ import styles from './VideoBox.scss';
 
 class VideoBox extends React.PureComponent {
 
-  
-  
+
+
 
   render() {
     return (
-      <div id="videoDiv"> 
+      <div id="videoDiv">
 
-        <div id="videoBlock1"> 
-        <img src="pattern1.jpg" alt="blah" className="rotate901">
-        </img>
+        <div id="videoBlock1">
+          <img src="pattern1.jpg" alt="Jonathan Bry" className="rotate901">
+          </img>
+          <img src="banner1.png" alt="Jonathan Bry" className="banner1">
+          </img>
           <video autoPlay muted="false" loop id='container1'>
             <source src="VDJ1Loop.mp4" type="video/mp4"></source>
-          </video> 
+          </video>
           <div id="videoMessage1">
             <h1 class="animated infinite headShake" id="HotSpot1">StarWars Character Wiki SPA</h1>
             <h2 class="spacer">MERN stack with a JSON server Backend</h2>
             <p class="videoClick" >
-            <a href="https://www.hotelsafloat.com/home-away.php">Click here and be impressed</a>
+              <a href="#" class="button hvr-grow">
+                Click here and be impressed
+              </a>
             </p>
             <button onClick={this.enableMute1}>
               M
-            </button> 
+            </button>
           </div>
         </div>
 
         <div id="videoBlock2">
-        <img src="pattern1.jpg" alt="blah" className="rotate902">
-        </img>
+          <img src="pattern1.jpg" alt="blah" className="rotate902">
+          </img>
           <video autoPlay muted="false" loop id='container2'>
             <source src="CraziestCrowd.mp4" type="video/mp4"></source>
-          </video> 
+          </video>
           <div id="videoMessage2">
             <h1 class="animated infinite pulse" id="HotSpot2">RaveMom Festival Assistant</h1>
             <h2 class="spacer">Made In A MEAN Stack</h2>
             <h3 class="spacer">HACKTILLDAWN finalist</h3>
             <p class="videoClick" >
-            <a href="https://www.hotelsafloat.com/home-away.php">Click here and be impressed</a>
+              <a href="https://www.hotelsafloat.com/home-away.php">Click here and be impressed</a>
             </p>
           </div>
         </div>
 
-        <div id="videoBlock3">   
-        <img src="pattern1.jpg" alt="blah" className="rotate903">
-        </img>
-        <img src="pattern1.jpg" alt="blah" className="rotate904">
-        </img>
+        <div id="videoBlock3">
+          <img src="pattern1.jpg" alt="blah" className="rotate903">
+          </img>
+          <img src="pattern1.jpg" alt="blah" className="rotate904">
+          </img>
           <video autoPlay muted loop id='container3'>
             <source src="Italy.mp4" type="video/mp4"></source>
-          </video> 
+          </video>
           <div id="videoMessage3">
-          <h1 class="animated infinite tada" id="HotSpot3">HotSpot</h1>
+            <h1 class="animated infinite tada" id="HotSpot3">HotSpot</h1>
             <h2 class="spacer">Co-working With a </h2>
             <h3 class="spacer">Connection</h3>
             <p class="videoClick" >
-            <a href="https://www.hotelsafloat.com/home-away.php">Click here and be impressed</a>
-            </p> 
+              <a href="https://www.hotelsafloat.com/home-away.php">Click here and be impressed</a>
+            </p>
           </div>
         </div>
       </div>
