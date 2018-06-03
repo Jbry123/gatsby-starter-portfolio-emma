2e6062a1aead8245f081e930b7c8ef848778ef94
Unknown <jonathanbry@comcast.net>
2018-06-03 14:27:32 -0700

    hopefully everything is back

--- src/components/VideoBox/VideoBox.jsx
+++ src/components/VideoBox/VideoBox.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import styles from './VideoBox.scss';
 
+import Crowd from './Crowd.mp4';
+import Italy from './Italy.mp4';
+import Starwars from './Starwars.mp4';
+
+import banner1 from './banner1.png';
+import pattern1 from './pattern1.jpg';
+import Projects from './Projects.png';
+
+
+
 {/* <div className="Starwars">
 <img className="Star" src={star} alt="star-logo" 
 />
@@ -17,12 +27,12 @@ class VideoBox extends React.PureComponent {
       <div id="videoDiv">
 
         <div id="videoBlock1">
-          <img src="pattern1.jpg" alt="Jonathan Bry" className="rotate901">
+          <img src={pattern1} alt="Jonathan Bry" className="rotate901">
           </img>
-          <img src="banner1.png" alt="Jonathan Bry" className="banner1">
+          <img src={banner1} alt="Jonathan Bry" className="banner1">
           </img>
           <video autoPlay muted="false" loop id='container1'>
-            <source src="VDJ1Loop.mp4" type="video/mp4"></source>
+            <source src={Starwars} type="video/mp4"></source>
           </video>
           <div id="videoMessage1">
             <h1 class="animated infinite headShake" id="HotSpot1">StarWars Character Wiki SPA</h1>
@@ -39,10 +49,10 @@ class VideoBox extends React.PureComponent {
         </div>
 
         <div id="videoBlock2">
-          <img src="pattern1.jpg" alt="blah" className="rotate902">
+          <img src={pattern1} alt="blah" className="rotate902">
           </img>
           <video autoPlay muted="false" loop id='container2'>
-            <source src="CraziestCrowd.mp4" type="video/mp4"></source>
+            <source src={Crowd} type="video/mp4"></source>
           </video>
           <div id="videoMessage2">
             <h1 class="animated infinite pulse" id="HotSpot2">RaveMom Festival Assistant</h1>
@@ -55,12 +65,12 @@ class VideoBox extends React.PureComponent {
         </div>
 
         <div id="videoBlock3">
-          <img src="pattern1.jpg" alt="blah" className="rotate903">
+          <img src={pattern1} alt="blah" className="rotate903">
           </img>
-          <img src="pattern1.jpg" alt="blah" className="rotate904">
+          <img src={pattern1} alt="blah" className="rotate904">
           </img>
           <video autoPlay muted loop id='container3'>
-            <source src="Italy.mp4" type="video/mp4"></source>
+            <source src={Italy} type="video/mp4"></source>
           </video>
           <div id="videoMessage3">
             <h1 class="animated infinite tada" id="HotSpot3">HotSpot</h1>
